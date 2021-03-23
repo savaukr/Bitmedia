65df141b5f91b473e759abc048bbc56c0b060696
savaukr <sava_ukr@ukr.net>
2021-03-23 07:50:58 +0200

    pagination

--- client/src/components/UsersList/UsersList.js
+++ client/src/components/UsersList/UsersList.js
@@ -4,23 +4,40 @@ import {Loader} from '../Loader/Loader'
 import {useHttp} from '../../hooks/http.hooks'
 import {useMessage} from '../../hooks/message.hook'
 import {UsersTable} from '../UsersTable/UsersTable'
+import {Pagination} from '../Pagination/Pagination'
 
 
 export const UsersList = () => {
+    const PER_PAGE = 50
+    const [usersCount, setUsersCount] = useState(0)
     const [users, setUsers] = useState([])
+    const [currentPage, setCurrentPage] = useState(1)
     const { request, error, clearError, loading } = useHttp()
     const message = useMessage()
 
-    const getUsersOnPage =useCallback( async (page=1, per_page=50) => {
+    const getUsersCount = useCallback(async () => {
+        try {
+           const usersCount = await request('api/user/count', 'POST')
+           setUsersCount(usersCount)
+        }catch(e) {
+
+        }
+    })
+
+    const getUsersOnPage = useCallback( async (page=1, per_page=PER_PAGE) => {
         try {
             const data = await request('/api/user', 'POST', {page, per_page})
             setUsers(data)
         } catch(e) {}
     }, [request])
 
+    useEffect(()=>{
+        getUsersCount()
+    },[])
+
     useEffect(() => {
-        getUsersOnPage()        
-    }, [getUsersOnPage]);
+        getUsersOnPage(currentPage)        
+    }, [getUsersOnPage, currentPage]);
 
     useEffect(() => {
         message(error)
@@ -32,9 +49,20 @@ export const UsersList = () => {
             <Loader />
         </div>
     )
+
+    const clickHandler = (event) => {
+        setCurrentPage(event.target.dataset.num)
+    }
+
     return (
         <div className="usersList-container">
             {!loading &&  <UsersTable  users= {users}/>}
+            {!loading && <Pagination 
+                            usersCount={usersCount}
+                            currentPage={currentPage}
+                            clickHandler={clickHandler}
+                            per_page ={PER_PAGE}
+                         />}
         </div>
     )
 }
